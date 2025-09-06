13b517a76891c4e8b2c6f5b4d2d507860090af33
AKDev32 <amankumar005562@gmail.com>
2025-09-06 23:12:50 +0530

    Hangman

--- src/components/Button/getButtonStyling.js
+++ src/components/Button/getButtonStyling.js
@@ -1,12 +1,15 @@
+
 function getButtonStyling(styleType) {
-    if (styleType === "primary") {
-        return "bg-blue-500 text-white"
+    if(styleType === "primary") {
+        return "bg-blue-500";
     } else if (styleType === "secondary") {
-        return "bg-gray-500"
-    } else if (styleType === "error") {
-        return "bg-red-500"
-    } else if (styleType === "warning") {
-        return "bg-yellow-500"
+        return "bg-gray-500";
+    } else if(styleType === "error") {
+        return "bg-red-500";
+    } else if(styleType === "success") {
+        return "bg-green-500";
+    } else if(styleType === "warning") {
+        return "bg-yellow-500";
     }
 }
 
